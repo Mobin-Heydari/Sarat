83e2f6baf0e6f91e9a46449d0a98b822bb296844
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-19 16:37:24 +0330

    The CreateContact view is added.

--- backend/Server/Contacts/views.py
+++ backend/Server/Contacts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import Response, APIView
+from rest_framework import status
 
-# Create your views here.
+from .serializers import ContactSerializer
+
+
+
+class CreateContact(APIView):
+    def post(self, request):
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            # Note: Corrected "Massage" to "Message" in the responses.
+            return Response({'Message': 'Contact created.'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
