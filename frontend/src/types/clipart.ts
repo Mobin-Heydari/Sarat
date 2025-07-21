724de128f5c824a63b825dedafa5d0a52f1a00d6
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-21 22:07:41 +0330

    Clipart type interface is added.

---
+++ frontend/src/types/clipart.ts
@@ -0,0 +1,17 @@
+
+export interface Clipart {
+    title: string;
+    slug: string;
+    description: string;
+
+    poster: string;
+    video: string;
+    music: string;
+
+    text: string;
+
+    views: number;
+
+    created_at: string;
+    updated_at: string;
+}
