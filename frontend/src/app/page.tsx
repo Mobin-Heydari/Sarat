110345f10d1ff1ee63489dcef0f0d8059d598ac2
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-21 10:44:27 +0330

    ScrollReveal componenet is added.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,18 +4,20 @@ import { Slider } from "@/components/ui/3DSlider";
 
 import { slideData } from "@/data";
 import { ContactForm } from "@/components/ContactForm";
+import { ScrollReveal } from "@/components/ScrollReveal";
 
 
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-between gap-12 h-auto w-full p-12">
+    <main className="flex flex-col justify-between gap-16 h-auto w-full p-12">
       {/* Slider */}
-      <section className="relative overflow-hidden w-full h-full py-20 flex flex-col justify-between gap-8" dir="ltr">
+      <section className="relative overflow-hidden w-full h-full py-20 flex flex-col justify-between gap-8 " dir="ltr">
         <h2 className="text-3xl font-bold text-center my-5 bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r from-primary-light via-selected-light to-secondary-light dark:from-primary-dark dark:via-selected-dark dark:to-selected-dark">برترین فعالیت های ما</h2>
         <p className="text-lg text-wrap text-right text-main-text-light dark:text-main-text-dark mb-3 p-3">توضبحات مختصر و مفید.</p>
         <Slider slides={slideData} />
       </section>
+      <ScrollReveal />
       <ContactForm />
     </main>
   );
