4917f6960f7a152c64ed65d95e2cac037334fb28
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-21 20:08:05 +0330

    The ClipartAdmin is added.

--- backend/Server/Cliparts/admin.py
+++ backend/Server/Cliparts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Clipart
+from .forms import ClipartForm
 
-# Register your models here.
+class ClipartAdmin(admin.ModelAdmin):
+    form = ClipartForm
+    list_display = ("title", "slug", "views", "created_at")
+    search_fields = ("title", "slug")
+    list_filter = ("created_at",)
+
+admin.site.register(Clipart, ClipartAdmin)
