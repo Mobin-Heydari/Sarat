e032cad4e5db980284edccb04b33c55187fe87a1
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-21 20:08:49 +0330

    The list and detail api view is added for the cliparts.

--- backend/Server/Cliparts/views.py
+++ backend/Server/Cliparts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.views import Response, APIView
+from rest_framework import status
+
+from .models import Clipart
+from .serializers import ClipartSerializer
+
+
+
+
+
+class ClipartsListApiView(APIView):
+
+    def get(self, request):
+        queryset = Clipart.objects.all()
+        serializer = ClipartSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ClipartDetailApiView(APIView):
+
+    def get(self, request, slug):
+        query = get_object_or_404(Clipart, slug=slug)
+        query.views = query.views + 1
+        query.save()
+        serializer = ClipartSerializer(query)
+        return Response(serializer.data, status=status.HTTP_200_OK)
