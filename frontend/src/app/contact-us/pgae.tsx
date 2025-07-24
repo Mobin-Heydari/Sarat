d77107aa638f77f5ab4864ac12a541cd39d7b0f8
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-24 16:44:02 +0330

    The contact us page is adedd.

---
+++ frontend/src/app/contact-us/pgae.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+import { BsViewList } from 'react-icons/bs';
+
+import { ContactForm } from "@/components/ContactForm";
+import HeroSection from "@/components/HeroSection";
+
+
+
+
+
+export default function ContactUs() {
+  return (
+    <main className="flex flex-col justify-between gap-16 h-auto w-full p-12">
+      {/* Hero */}
+      <HeroSection
+        title="تماس با ما"
+        mainText="برای ارتباط و همکاری فرم را پر کنید"
+        subText="به مجموعه ی صراط آمدید"
+        buttonTitle="درباره ی ما"
+        buttonIcon={ <BsViewList /> }
+        buttonPosition="right"
+        buttonUrl="/cliparts"
+      />
+      <ContactForm />
+    </main>
+  );
+}
