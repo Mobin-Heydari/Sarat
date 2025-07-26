308370c4f76216be9a4550c65599552448013b1f
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-26 12:24:21 +0330

    The list and detail view added for the stories.

--- backend/Server/Stories/views.py
+++ backend/Server/Stories/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.views import Response, APIView
+from rest_framework import status
+
+from .models import Story
+from .serializers import StorySerializer
+
+
+
+
+
+class StoriesListApiView(APIView):
+
+    def get(self, request):
+        queryset = Story.objects.all()
+        serializer = StorySerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class StoryDetailApiView(APIView):
+
+    def get(self, request, slug):
+        query = get_object_or_404(Story, slug=slug)
+        query.views = query.views + 1
+        query.save()
+        serializer = StorySerializer(query)
+        return Response(serializer.data, status=status.HTTP_200_OK)
