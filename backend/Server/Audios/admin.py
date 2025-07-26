9e5c951ffbea3cac820b5e14eccd3507c6a33af6
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-26 15:22:55 +0330

    The Audio admin panel is customed.

--- backend/Server/Audios/admin.py
+++ backend/Server/Audios/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Audio
 
-# Register your models here.
+
+
+@admin.register(Audio)
+class AudioAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "slug",
+        "views",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "created_at",
+        "updated_at",
+    )
+    search_fields = (
+        "title",
+        "slug",
+    )
+    prepopulated_fields = {"slug": ("title",)}
+    readonly_fields = (
+        "views",
+        "created_at",
+        "updated_at",
+    )
+    ordering = ("-created_at",)
