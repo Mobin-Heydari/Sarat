31298eda19be848abd3290f918f52135b4672cff
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-26 11:38:21 +0330

    The Stories admin is added and customized.

--- backend/Server/Stories/admin.py
+++ backend/Server/Stories/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Story, StoryVideos
 
-# Register your models here.
+
+class StoryVideosInline(admin.TabularInline):
+    model = StoryVideos
+    extra = 1
+    fields = ("title", "slug", "video", "created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "views", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("title", "slug")
+    prepopulated_fields = {"slug": ("title",)}
+    readonly_fields = ("views", "created_at", "updated_at")
+    inlines = [StoryVideosInline]
+    ordering = ("-created_at",)
+
+
+@admin.register(StoryVideos)
+class StoryVideosAdmin(admin.ModelAdmin):
+    list_display = ("story", "title", "slug", "created_at", "updated_at")
+    list_filter = ("created_at",)
+    search_fields = ("story__title", "title", "slug")
+    prepopulated_fields = {"slug": ("title",)}
+    readonly_fields = ("created_at", "updated_at")
+    ordering = ("-created_at",)
