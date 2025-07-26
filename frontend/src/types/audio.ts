720e1f2e24b6dcc5657fec00cb3bc67d1d32f054
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-26 16:21:07 +0330

    The Audio type is added.

---
+++ frontend/src/types/audio.ts
@@ -0,0 +1,16 @@
+
+export interface Audio {
+    title: string;
+    slug: string;
+    description: string;
+
+    poster: string;
+    music: string;
+
+    text: string;
+
+    views: number;
+
+    created_at: string;
+    updated_at: string;
+}
