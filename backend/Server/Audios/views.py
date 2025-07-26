9ee0fc6c8257456c9d43f26619763175b88c011f
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-26 15:27:45 +0330

    The AudiosListApiView and AudioDetailApiView are added to provide the list and retrieve audios.

--- backend/Server/Audios/views.py
+++ backend/Server/Audios/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.views import Response, APIView
+from rest_framework import status
+
+from .models import Audio
+from .serializers import AudioSerializer
+
+
+
+
+
+class AudiosListApiView(APIView):
+
+    def get(self, request):
+        queryset = Audio.objects.all()
+        serializer = AudioSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class AudioDetailApiView(APIView):
+
+    def get(self, request, slug):
+        query = get_object_or_404(Audio, slug=slug)
+        query.views = query.views + 1
+        query.save()
+        serializer = AudioSerializer(query)
+        return Response(serializer.data, status=status.HTTP_200_OK)
