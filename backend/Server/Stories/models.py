88df24f730ca02a21f07389db8fec64fc66f1bfe
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-26 11:12:21 +0330

    The Story and StoryVideos model is added.

--- backend/Server/Stories/models.py
+++ backend/Server/Stories/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+class Story(models.Model):
+    
+    title = models.CharField(verbose_name="عنوان", max_length=250)
+    slug = models.SlugField(verbose_name="اسلاگ", max_length=250, unique=True, primary_key=True)
+
+    poster = models.ImageField(verbose_name="پوستر", upload_to="stories/posters")
+
+    views = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(verbose_name="تاریخ ایجاد", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="تاریخ بهروزرسانی", auto_now=True)
+
+
+
+class StoryVideos(models.Model):
+
+    story = models.ForeignKey(
+        Story,
+        on_delete=models.CASCADE,
+        related_name="videos",
+        verbose_name="استوری"
+    )
+
+    video = models.FileField(verbose_name="ویدیو", upload_to="stories/posters")
+
+    created_at = models.DateTimeField(verbose_name="تاریخ ایجاد", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="تاریخ بهروزرسانی", auto_now=True)
