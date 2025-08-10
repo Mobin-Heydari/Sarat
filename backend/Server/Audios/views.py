1ba89589db055a9c5bde7301cc9b29baff2958fa
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-08-10 14:17:08 +0330

    The AudioViewSet is added.

--- backend/Server/Audios/views.py
+++ backend/Server/Audios/views.py
@@ -1,28 +1,35 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework.views import Response, APIView
+from rest_framework.views import Response
+from rest_framework.viewsets import ViewSet
 from rest_framework import status
 
-from .models import Audio
 from .serializers import AudioSerializer
+from .models import Audio
 
 
 
+class AudioViewSet(ViewSet):
+    lookup_field = 'slug'
 
-
-class AudiosListApiView(APIView):
-
-    def get(self, request):
-        queryset = Audio.objects.all()
+    def list(self, request, *args, **kwargs):
+        instance = Audio.objects.order_by('-views')
+        serializer = AudioSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, slug, *args, **kwargs):
+        instance = get_object_or_404(Audio, slug=slug)
+        instance.views += 1
+        instance.save()
+        serializer = AudioSerializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def famous_musics(self, request):
+        queryset = Audio.objects.order_by('-views')[:6]
         serializer = AudioSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class AudioDetailApiView(APIView):
-
-    def get(self, request, slug):
-        query = get_object_or_404(Audio, slug=slug)
-        query.views = query.views + 1
-        query.save()
-        serializer = AudioSerializer(query)
+    
+    def new_musics(self, request):
+        queryset = Audio.objects.order_by('-published_date')[:6]
+        serializer = AudioSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
