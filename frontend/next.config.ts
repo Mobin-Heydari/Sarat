25cc2fd7f3f6fdda23862752b3037895e417f65a
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-08-13 11:21:23 +0330

    The next config file updated.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_SERVER_BASE_API_URL: process.env.NEXT_PUBLIC_SERVER_BASE_API_URL,
+    NEXT_PUBLIC_MEDIA_URL: process.env.NEXT_PUBLIC_MEDIA_URL,
+  },
+
   images: {
     domains: ['127.0.0.1', 'localhost'],
-  },
+  }
 };
 
 export default nextConfig;
