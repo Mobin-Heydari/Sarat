cd183c9871baaccc95e7ac53a1a0ac92d6f6f07a
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-09-04 12:26:57 +0330

    The Funny and FunnyContent models is added.

--- backend/Server/Funny/models.py
+++ backend/Server/Funny/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+
+
+class Funny(models.Model):
+    title = models.CharField(verbose_name="عنوان", max_length=250)
+    slug = models.CharField(verbose_name="اسلاگ", max_length=250, unique=True, primary_key=True)
+    description = models.TextField(verbose_name="توضیحات")
+
+    poster = models.ImageField(verbose_name="پوستر", upload_to="funny/posters/")
+    video = models.URLField(verbose_name="آدرس ویدیو")
+
+    text = RichTextField(verbose_name="متن نماهنگ")
+
+    views = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(verbose_name="تاریخ ایجاد", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="تاریخ بهروزرسانی", auto_now=True)
+
+    class Meta:
+        verbose_name = "نماهنگ"
+        verbose_name_plural = "نماهنگ ها"
+
+    
+    def __str__(self):
+        return self.title
+    
+
+class FunnyContent(models.Model):
+
+    funny =  models.ForeignKey(
+        Funny,
+        on_delete=models.CASCADE,
+        verbose_name="بامزه",
+        related_name="content"
+    )
+
+    title = models.CharField(verbose_name="عنوان", max_length=250, null=True, blank=True)
+
+    image = models.ImageField(verbose_name="تصویر", upload_to="funny/content/image/", null=True, blank=True)
+    video = models.URLField(verbose_name="آدرس ویدیو", null=True, blank=True)
+
+    content = RichTextField(verbose_name="متن نماهنگ", null=True, blank=True)
+
+    created_at = models.DateTimeField(verbose_name="تاریخ ایجاد", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="تاریخ بهروزرسانی", auto_now=True)
+
+
+    def __str__(self):
+        return self.funny.title
