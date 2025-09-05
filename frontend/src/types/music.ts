a9631be3da24c4ad205927520b05590652bea474
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-09-05 14:17:15 +0330

    The Music type inteface is added.

---
+++ frontend/src/types/music.ts
@@ -0,0 +1,16 @@
+
+export interface Music {
+    title: string;
+    slug: string;
+    description: string;
+
+    poster: string;
+    music: string;
+
+    text: string;
+
+    views: number;
+
+    created_at_jalali: string;
+    updated_at_jalali: string;
+}
