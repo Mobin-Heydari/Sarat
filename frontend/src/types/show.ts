f7912da6950e22f88efd0b5d375034819202a3bc
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-09-13 16:12:16 +0330

    The Show type is added.

---
+++ frontend/src/types/show.ts
@@ -0,0 +1,11 @@
+export interface Show {
+  title: string;
+  slug: string;
+  description: string;
+  poster: string;
+  video: string;
+  text: string;
+  views: number;
+  created_at_jalali: string;
+  updated_at_jalali: string;
+}
