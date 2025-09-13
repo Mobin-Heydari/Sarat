9de8b384e5345aac54f336d9ab42bc4aa52fcc44
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-09-13 14:27:21 +0330

    The ShowViewSet  is added.

--- backend/Server/Lives/views.py
+++ backend/Server/Lives/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+from rest_framework import status
 
-# Create your views here.
+from .models import Show
+from .serializers import ShowSerializer
+
+
+class ShowViewSet(ViewSet):
+    lookup_field = 'slug'
+
+    def list(self, request, *args, **kwargs):
+        queryset = Show.objects.order_by('-views')
+        serializer = ShowSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, slug=None, *args, **kwargs):
+        instance = get_object_or_404(Show, slug=slug)
+        instance.views += 1
+        instance.save(update_fields=['views'])
+        serializer = ShowSerializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def famous_shows(self, request):
+        queryset = Show.objects.order_by('-views')[:6]
+        serializer = ShowSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def new_shows(self, request):
+        queryset = Show.objects.order_by('-created_at')[:6]
+        serializer = ShowSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
