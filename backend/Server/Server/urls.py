acaa3c687517486a363146c296ad7db2475aa4b3
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-09-19 10:15:42 +0330

    The  urls  are updated.

--- backend/Server/Server/urls.py
+++ backend/Server/Server/urls.py
@@ -8,10 +8,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('contacts/', include('Contacts.urls', namespace="Contacts")),
-    path('cliparts/', include('Cliparts.urls', namespace="Cliparts")),
-    path('stories/', include('Stories.urls', namespace="Stories")),
-    path('audios/', include('Audios.urls', namespace="Audios")),
-    path('funny/', include('Funny.urls', namespace="Funny")),
-    path('lives/', include('Lives.urls', namespace="Lives")),
+    path('api/v1/contacts/', include('Contacts.urls', namespace="Contacts")),
+    path('api/v1/cliparts/', include('Cliparts.urls', namespace="Cliparts")),
+    path('api/v1/stories/', include('Stories.urls', namespace="Stories")),
+    path('api/v1/audios/', include('Audios.urls', namespace="Audios")),
+    path('api/v1/funny/', include('Funny.urls', namespace="Funny")),
+    path('api/v1/lives/', include('Lives.urls', namespace="Lives")),
 ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
