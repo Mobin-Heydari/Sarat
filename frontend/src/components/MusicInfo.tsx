b1eb6f17d02acf61c08bf7fb596eaa9987a9512b
Mobin-Heydari <mobinheydari.developer@gmail.com>
2025-11-10 00:16:30 +0330

    src cleaned.

--- frontend/src/components/MusicInfo.tsx
+++ frontend/src/components/MusicInfo.tsx
@@ -6,6 +6,7 @@ import { motion } from 'framer-motion';
 import { Music } from '@/types/music';
 
 export default function MusicHeroBanner({ data }: { data: Music }) {
+  console.log(`${process.env.NEXT_PUBLIC_MEDIA_URL}${data.poster}`)
   return (
     <div className="relative w-full h-[500px] md:h-[600px] rounded-3xl overflow-hidden shadow-xl">
       {/* Background Poster */}
